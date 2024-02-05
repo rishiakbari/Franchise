f3fadea17120d4efa32122ec3ff3fd2b2f6b448f
rishiakbari <akbaririshi6@gmail.com>
2024-02-05 12:47:54 +0530

    icon widget add

--- lib/widgets/custom_text_button.dart
+++ lib/widgets/custom_text_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../utils/colorz.dart';
 
 class CustomTextButton extends StatelessWidget {
@@ -15,11 +14,14 @@ class CustomTextButton extends StatelessWidget {
     this.isGradient = false,
     this.isUnderlineText = false,
     this.padding,
+    this.icon,
+    this.width,
   });
 
   final Function()? onPressed;
   final String text;
   final double? minWidth;
+  final double? width;
   final Color? textColor;
   final Color? backgroundColor;
   final Gradient? gradient;
@@ -27,6 +29,7 @@ class CustomTextButton extends StatelessWidget {
   final bool isGradient;
   final bool isUnderlineText;
   final EdgeInsetsGeometry? padding;
+  final Widget? icon;
 
   @override
   Widget build(BuildContext context) {
@@ -49,22 +52,50 @@ class CustomTextButton extends StatelessWidget {
       child: MaterialButton(
         materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-        padding: padding ?? const EdgeInsets.symmetric(
-          vertical: 8.0,
-          horizontal: 12.0,
-        ),
+        padding: padding ??
+            const EdgeInsets.symmetric(
+              vertical: 8.0,
+              horizontal: 12.0,
+            ),
         height: 40.0,
         minWidth: fitted ? null : minWidth ?? double.infinity,
         onPressed: onPressed,
-        child: Text(
-          text,
-          style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: textColor ?? Colors.black,
-                fontWeight: FontWeight.w600,
-                decoration: isUnderlineText ? TextDecoration.underline : null,
-                decorationColor: textColor ?? Colors.black,
-              ),
-        ),
+        child: icon != null
+            ? SizedBox(
+                width: fitted ? null : width ?? double.infinity,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (icon != null) icon!,
+                    SizedBox(
+                      width: icon != null ? 10 : 0,
+                    ),
+                    FittedBox(
+                      child: Text(
+                        text,
+                        style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                              color: textColor ?? Colors.black,
+                              fontWeight: FontWeight.w600,
+                              decoration: isUnderlineText
+                                  ? TextDecoration.underline
+                                  : null,
+                              decorationColor: textColor ?? Colors.black,
+                            ),
+                      ),
+                    ),
+                  ],
+                ),
+              ):FittedBox(
+                  child: Text(
+                  text,
+                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        color: textColor ?? Colors.black,
+                        fontWeight: FontWeight.w600,
+                        decoration: isUnderlineText ? TextDecoration.underline : null,
+                        decorationColor: textColor ?? Colors.black,
+                      ),
+                   ),
+                ),
       ),
     );
   }
