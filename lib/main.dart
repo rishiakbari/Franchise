d09e707751af68d22bcbddd4dbe301a55c2efceb
rishiakbari <akbaririshi6@gmail.com>
2024-02-05 12:49:41 +0530

    routes set

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:franchise/screens/add_coupon_screen.dart';
+import 'package:franchise/screens/bottom_navigationb_bar_screen.dart';
+import 'package:franchise/screens/completed_order_screen.dart';
+import 'package:franchise/screens/coupon_screen.dart';
+import 'package:franchise/screens/create_profile_screen.dart';
+import 'package:franchise/screens/customer_lead_screen.dart';
+import 'package:franchise/screens/dashboard.dart';
+import 'package:franchise/screens/my_lead_screen.dart';
+import 'package:franchise/screens/order_screen.dart';
+import 'package:franchise/screens/splash_screen.dart';
+import 'package:franchise/screens/wallet_screen.dart';
+import 'package:franchise/utils/app_theme.dart';
+import 'screens/add_new_order_screen.dart';
 import 'screens/login_screen.dart';
+import 'screens/wallet_history_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -18,12 +33,25 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      theme: buildThemeData(context),
       title: 'Franchise',
-      home: const LoginScreen(), 
+      home: const SplashScreen(), 
       initialRoute: '/',
       routes: {
+        SplashScreen.routeName :(context) => const SplashScreen(),
         LoginScreen.routeName :(context) => const LoginScreen(),
+        CreateProfileScreen.routeName :(context) => const CreateProfileScreen(),
+        DashBoardScreen.routeName :(context) => const DashBoardScreen(),
+        OrderScreen.routeName :(context) => const OrderScreen(),
+        AddNewOrderScreen.routeName :(context) => const AddNewOrderScreen(), 
+        CompletedOrderScreen.routeName :(context) => const CompletedOrderScreen(),
+        CouponScreen.routeName :(context) => const CouponScreen(),
+        CustomDrawer.routeName :(context) => const CustomDrawer(),
+        AddCouponScreen.routeName :(context) => const AddCouponScreen(),
+        CustomerLeadScreen.routeName :(context) => const CustomerLeadScreen(),
+        MyLeadScreen.routeName :(context) => const MyLeadScreen(),
+        WalletScreen.routeName :(context) => const WalletScreen(),
+        WalletHistoryScreen.routeName :(context) => const WalletHistoryScreen(),
       }
     );
   }
